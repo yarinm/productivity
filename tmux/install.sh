69a71c187954375ec80a8463574e59a4d2b9cd35
Yarin Miran <yarin@Yarins-MBP.lan>
2020-03-02 00:34:41 +0200

    Added submodules pull

--- tmux/install.sh
+++ tmux/install.sh
@@ -8,6 +8,10 @@ fi
 
 read -p "Using $repo_path -- Press enter to continue..." REPLY
 
+echo "Pulling submodules"
+git submodule update --init --recursive
 echo "source $repo_path/tmux/tmux.conf" > ~/.tmux.conf
 
 ln -sf "$repo_path/tmux" ~/.tmux
+
+echo "Done! Don't forget to run Ctrl+a, I to install plugins"
